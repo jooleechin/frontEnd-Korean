53ece639956899480341459473cd1f7129e2229a
Patrick Coury <couryp@gmail.com>
2018-03-27 16:07:52 -0700

    thanks charlie

--- src/components/learn.js
+++ src/components/learn.js
@@ -6,32 +6,35 @@ import { Row, Col, CardPanel } from 'react-materialize'
 
 class Learn extends Component {
   state = {
-     info: []
+     info: [],
+     learn: '',
+     symbol: '',
+     pronounciation: ''
    }
 
    componentDidMount() {
      axios.get(`http://localhost:3000/learn/1`)
        .then((response) => {
-         console.log('test', response)
-         console.log('snarf', response.data.learn)
+         console.log('test', response.data.learn)
          const info = response.data;
-         this.setState({ info })
+         this.setState({
+           symbol: info.learn.symbol,
+           pronounciation: info.learn.pronounciation
+         })
        })
    }
   render() {
+    console.log('state', this.state)
     return (
       <div>
       <Row>
           <Col s={12} m={5}>
-
               <CardPanel className="teal lighten-4 black-text">
                   <span>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks ㅏㅏㅏokok.</span>
               </CardPanel>
-
           </Col>
       </Row>
-      {console.log(this.state.info.learn)}
-
+        <div>{this.state.symbol} {this.state.pronounciation}</div>
       </div>
     )
   }
