2b3e0467ec46e3c86282249ae6aec0e151769429
Patrick Coury <couryp@gmail.com>
2018-03-27 15:13:00 -0700

    working on learn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import Header from './components/header'
+import Learn from './components/learn'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Learn />
       </div>
     );
   }
---
+++ src/components/learn.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import { Row, Col, CardPanel } from 'react-materialize'
+
+
+class Learn extends Component {
+  state = {
+     info: []
+   }
+
+   componentDidMount() {
+     axios.get(`http://localhost:3000/learn/1`)
+       .then((response) => {
+         console.log('test', response)
+         console.log('snarf', response.data.learn.symbol)
+         const info = response.data;
+         this.setState({ info })
+       })
+   }
+  render() {
+    return (
+      <div>
+      <Row>
+          <Col s={12} m={5}>
+
+              <CardPanel className="teal lighten-4 black-text">
+                  <span>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks ㅏㅏㅏokok {this.state.info.learn.symbol}.</span>
+              </CardPanel>
+
+          </Col>
+      </Row>
+      {console.log(this.state.info.learn)}
+
+      </div>
+    )
+  }
+}
+
+export default Learn
