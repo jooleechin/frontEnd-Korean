efcc211e94df2436b90983a62ac0daaae7f0c2a9
Joolee Chin <jooleechin@gmail.com>
2018-03-27 11:01:02 -0700

    starting component

---
+++ src/components/header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+  }
+
+  render() {
+    return (
+      <div>New component</div>
+    )
+  }
+}
+
+export default Header
