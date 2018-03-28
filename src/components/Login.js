9c363be3d26aaa9c57bd0dfbf0227615b57685cc
Joolee Chin <jooleechin@gmail.com>
2018-03-28 14:50:18 -0700

    added proxy and udated axious call

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Login extends Component {
+  loginCheck(e) {
+    e.preventDefault()
+    let email = e.target.email.value
+    let password = e.target.password.value
+    axios.post('/login', { email, password })
+    .then (data => {
+      if (data.data.matches) {
+        sessionStorage.setItem('u')
+      }
+    })
+  }
+
+  // loginCheck(email, password) {
+  //   axios.post(`${baseURL}/login`, {email, password})
+  //   .then (data => {
+  //     console.log(data)
+  //   })
+  // }
+
+
+
   render() {
     return(
       <div>
-        <label for="email">email: </label>
-          <input id="email" type="email" />
-        <label for="password">password: </label>
-          <input id="password" type="password" />
-        <input type="submit" value="submit" />
+        <form id="formLogin" onSubmit={this.loginCheck}>
+          <label for="email">email: </label>
+            <input name="email" id="email" type="email" />
+          <label for="password">password: </label>
+            <input name="password" id="password" type="password" />
+          <input type="submit" value="submit"/>
+        </form>
       </div>
     )
   }
